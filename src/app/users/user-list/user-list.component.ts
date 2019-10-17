5492e46b4c9f697aadd8dbc80928f095e35c3d54
Alaeddine Thamine <alaeddine.thamine.prestaire@sfr.com>
2019-10-17 15:24:27 +0200

    Lack of memory Subscribe with unsubscribe

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { timer } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, timer } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 
 import { UsersService } from '../shared/users.service';
 import { User } from '../shared/user.model';
@@ -10,8 +10,9 @@ import { User } from '../shared/user.model';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.sass']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy {
   users: User[];
+  unsubscribe$ = new Subject<void>();
 
   constructor(
     private service: UsersService
@@ -23,7 +24,13 @@ export class UserListComponent implements OnInit {
 
   fetchUsers() {
     timer(0, 10000).pipe(
+      takeUntil(this.unsubscribe$),
       switchMap(() => this.service.fetchUsers())
     ).subscribe((users: User[]) => this.users = users);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
