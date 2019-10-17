d7b75e10c19903ae983d63973e43889638292810
Alaeddine Thamine <alaeddine.thamine.prestaire@sfr.com>
2019-10-17 15:27:06 +0200

    Lack of memory Subscribe with pipe async

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject, timer } from 'rxjs';
-import { switchMap, takeUntil } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { UsersService } from '../shared/users.service';
 import { User } from '../shared/user.model';
@@ -10,9 +10,8 @@ import { User } from '../shared/user.model';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.sass']
 })
-export class UserListComponent implements OnInit, OnDestroy {
-  users: User[];
-  unsubscribe$ = new Subject<void>();
+export class UserListComponent implements OnInit {
+  users$: Observable<User[]>;
 
   constructor(
     private service: UsersService
@@ -23,14 +22,6 @@ export class UserListComponent implements OnInit, OnDestroy {
   }
 
   fetchUsers() {
-    timer(0, 10000).pipe(
-      takeUntil(this.unsubscribe$),
-      switchMap(() => this.service.fetchUsers())
-    ).subscribe((users: User[]) => this.users = users);
-  }
-
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
+    this.users$ = timer(0, 10000).pipe(switchMap(() => this.service.fetchUsers()));
   }
 }
